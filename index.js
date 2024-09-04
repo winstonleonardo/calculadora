3c330c1d8a3be2bcfdb52789cb9e50994fb68fff
winston leonardo <winstonleonardo@yahoo.com.br>
2024-09-04 12:54:06 -0300

    minha calculadora

---
+++ index.js
@@ -0,0 +1,9 @@
+let resultado = soma(20, 10)
+console.log(" O herói tem saldo de vitórias " + resultado)
+let nivel =("esta no nivel de ferro")
+console.log(nivel)
+
+function soma(vitoria , derrota){
+    let somatorio = vitoria - derrota
+return somatorio 
+}
